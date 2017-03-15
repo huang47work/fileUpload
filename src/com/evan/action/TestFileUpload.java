08eb5ca9e2decd3806a609ec5c73a3911038f239
huang47work <377298496@qq.com>
2017-03-15 16:45:49 +0800

    init push

---
+++ src/com/evan/action/TestFileUpload.java
@@ -0,0 +1,17 @@
+package com.evan.action;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by huangsiqian on 2017/3/15 0015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:config/spring.xml"})
+public class TestFileUpload {
+
+
+
+}
